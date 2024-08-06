4701d0df897d6b7edd8c9a9e0f39e54c6a4dd83d
Ruba-Salih <rubasalih900@gmail.com>
2024-08-07 02:43:07 +0300

    fixBug

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# api/v1/views/__init__.py
-
+"""
+Contains the blueprint for the API
+"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
@@ -8,3 +9,8 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
