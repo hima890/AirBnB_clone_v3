3a2b4f8e144fe9d5f235d8793b5ad9312ddb2e6c
Ruba-Salih <rubasalih900@gmail.com>
2024-08-07 02:05:10 +0300

    .

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask routes file logic"""
-from flask import jsonify
+from flask import jsonify, request
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
