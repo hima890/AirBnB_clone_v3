ecd49a9c6b8200795c66ab4fa9ec054e8e925126
Ruba-Salih <rubasalih900@gmail.com>
2024-08-07 01:39:00 +0300

    .

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-"""Flask routes file logic"""
+'''Contains the index view for the API.'''
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    '''Returns the status of the API'''
-    return jsonify({
-        "status": "OK"
-        })
+    """ Returns JSON """
+    return jsonify(status="OK")
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
-    """Returns the number of each object by type"""
-    stats = {
-        'amenities': storage.count(Amenity),
-        'cities': storage.count(City),
-        'places': storage.count(Place),
-        'reviews': storage.count(Review),
-        'states': storage.count(State),
-        'users': storage.count(User)
-    }
-    return jsonify(stats)
+def stat():
+    """returns the number of each objects by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
