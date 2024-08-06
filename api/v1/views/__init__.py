e6cded23c6c3db51d3e9eaf201786dcdd8dc3119
Ruba-Salih <rubasalih900@gmail.com>
2024-08-07 01:52:58 +0300

    FixBug

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# api/v1/views/__init__.py
 """
 Contains the blueprint for the API
 """
+
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
